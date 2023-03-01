93a20d9883273784bb311a2ff782bb62d91d8972
SAAD EL MANSOURI <saadelman1991@gmail.com>
2023-03-01 13:48:52 +0100

    fix right

--- resources/views/livewire/recettes/recette-form.blade.php
+++ resources/views/livewire/recettes/recette-form.blade.php
@@ -1,8 +1,8 @@
 <tr><td colspan="6">
     <form action="" wire:submit.prevent="save">
         <label for="name">name</label>
-        <input type="text" wire:model.defer="recette.name">
-        <button class="mybutton" type="submit">Save</button>
+        <input class="mybuttonW" type="text" wire:model.defer="recette.name">
+        <button class="mybuttonW" type="submit">Save</button>
         @error("recette.name")
         {{$message}}
         @enderror
--- resources/views/livewire/template/header.blade.php
+++ resources/views/livewire/template/header.blade.php
@@ -1,10 +1,12 @@
 <div class="Header backcolor">
     <div style="height:100%;width:100%;display:flex;;flex-direction:row;gap:10%;justify-content:space-between;align-items: center;">
-        <div style="height:30%;width:60%;border-radius: 30px;background-color:#FFFFFF">
-
+        <div style="height:30%;width:50%;border-radius: 30px;background-color:#FFFFFF;display:flex;justify-content:space-around;align-content:center;flex-wrap:wrap">
+            <img  src="{{ asset('images/svg/search.svg') }}" alt="" height="30px" style="margin-left:5px;border-radius: 30px">
+            Search by food name
         </div>
-        <div style="height:30%;width:5%;border-radius: 30px;background-color:#FFFFFF">
-
+        <div style="height:30%;width:15%;border-radius: 30px;background-color:#FFFFFF;display:flex;justify-content:space-around;align-content:center;flex-wrap:wrap">
+            <img  src="{{ asset('images/svg/filter.svg') }}" alt="" height="20px" style="margin-left:5px;border-radius: 30px" >
+            Filter
         </div>
     </div>
 
