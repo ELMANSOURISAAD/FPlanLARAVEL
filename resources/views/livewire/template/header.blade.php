b49b6320ea3055ee54816b714b73adb67735e7c4
SaadLapTop <saadelman1991@gmail.com>
2023-03-01 17:39:07 +0100

    fix front ag

--- resources/views/livewire/template/header.blade.php
+++ resources/views/livewire/template/header.blade.php
@@ -1,16 +1,18 @@
 <div class="Header backcolor">
     <div style="height:100%;width:100%;display:flex;;flex-direction:row;gap:10%;justify-content:space-between;align-items: center;">
-        <div style="font-size: 1em;
+        <div style="color: rgba(0, 0, 0, 0.5);font-size: 1em;
         margin-top: 0;
-        color: #AFA8CA;height:30%;width:50%;border-radius: 30px;background-color:#FFFFFF;display:flex;justify-content:space-around;align-content:center;flex-wrap:wrap">
-            <img  src="{{ asset('images/svg/search.svg') }}" alt="" height="30px" style="margin-left:5px;border-radius: 30px">
-            Search by food name
+        ;height:30%;width:50%;border-radius: 30px;background-color:#FFFFFF;display:flex;align-content:center;flex-wrap:wrap">
+            <img  src="{{ asset('images/svg/search.svg') }}" alt="" height="30px" style="margin-left:3px">
+            <span>Chercher par recette</span>
+            
         </div>
-        <div style="font-size: 1em;
+        <div style="
+        color: rgba(0, 0, 0, 0.5);font-size: 1em;
         margin-top: 0;
-        color: #AFA8CA;height:30%;width:15%;border-radius: 30px;background-color:#FFFFFF;display:flex;justify-content:space-around;align-content:center;flex-wrap:wrap">
-            <img  src="{{ asset('images/svg/filter.svg') }}" alt="" height="20px" style="margin-left:5px;border-radius: 30px" >
-            Filter
+        height:30%;width:15%;border-radius: 30px;background-color:#FFFFFF;display:flex;align-content:center;flex-wrap:wrap">
+            <img  src="{{ asset('images/svg/filter.svg') }}" alt="" height="20px" style="margin-left:3px" >
+            <span>Trier</span>
         </div>
     </div>
 
