7ef5d1e905b0fbc67f6afee551ef5275cb566d39
SaadLapTop <saadelman1991@gmail.com>
2023-03-02 01:54:54 +0100

    Suggestions recette rightbar2

--- resources/views/livewire/template/rightbar.blade.php
+++ resources/views/livewire/template/rightbar.blade.php
@@ -37,11 +37,13 @@
         <img src="{{ asset('images/food.png') }}" alt="" height="100" width="100">
         <h2 style="color:#3B315D;letter-spacing: -1px;font-size:larger"> Omelette </h2>
         <p style="color:#3F3560;font-size:smaller"> Cout : X </p>
+       
     </div>
     <div style="background-color: #FFF9F3;border-radius:5px;height:100%;width:50%;display:flex;justify-content:center;flex-direction:column;align-items: center;">
         <img src="{{ asset('images/food.png') }}" alt="" height="100" width="100">
-        <h2 style="color:#3B315D;letter-spacing: -1px;font-size:larger"> Omelette </h2>
+        <h2 style="color:#3B315D;letter-spacing: -1px;font-size:larger"> Tarte citron </h2>
         <p style="color:#3F3560;font-size:smaller"> Cout : X </p>
+        
     </div>
 
   </div>
